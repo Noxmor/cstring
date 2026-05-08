21f230f0e9fccb1c19a0d51d56d432aa844c5c52
Noxmor <lord.noxmor@gmail.com>
2026-05-09 01:58:11 +0200

    Add macros for easy definitions of strings with interesting sizes

--- tests/test.h
+++ tests/test.h
@@ -6,6 +6,23 @@
 
 #include <stdlib.h>
 
+#define TEST_INIT_STRING(str, capacity) \
+    do \
+    { \
+        for (size_t _i = 0; _i < capacity; ++_i) str[_i] = '#'; \
+        str[capacity - 1] = '\0'; \
+    } while (0)
+
+#define TEST_MAKE_STRING(name, capacity) \
+    char name[capacity]; \
+    TEST_INIT_STRING(name, capacity)
+
+#define TEST_MAKE_SSO_STRING(name) \
+    TEST_MAKE_STRING(name, CSTRING_SSO_CAPACITY - 1)
+
+#define TEST_MAKE_HEAP_STRING(name) \
+    TEST_MAKE_STRING(name, CSTRING_SSO_CAPACITY + 1)
+
 #define TEST_ASSERT(x) \
     if (!(x)) exit(EXIT_FAILURE)
 

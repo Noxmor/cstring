3268ac7286eb994c440fbd0187a11989533b4809
Noxmor <lord.noxmor@gmail.com>
2026-05-09 16:58:25 +0200

    Add test for function cstring_capacity()

---
+++ tests/capacity.c
@@ -0,0 +1,23 @@
+#include "test.h"
+
+int main()
+{
+    for (size_t i = 1; i < CSTRING_SSO_CAPACITY * 2; ++i)
+    {
+        TEST_MAKE_STRING(str, i);
+
+        cstring_t s = cstring_from(str);
+
+        TEST_ASSERT(cstring_capacity(&s) == i || (cstring_capacity(&s) == CSTRING_SSO_CAPACITY && i <= CSTRING_SSO_CAPACITY));
+
+        cstring_free(&s);
+    }
+
+    cstring_t s = cstring_from("");
+
+    TEST_ASSERT(cstring_capacity(&s) == CSTRING_SSO_CAPACITY);
+
+    cstring_free(&s);
+
+    return EXIT_SUCCESS;
+}
